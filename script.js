0fe5738748fb2b4e5801f26d3c1c3d7aa50d2556
New dirac <98841909+Angryyy@users.noreply.github.com>
2022-06-05 11:45:04 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const DivImg = document.getElementById("DivImg")
+const imagenCompleta = document.getElementById("complete");
+function cerrar(){
+    DivImg.style.display="none";
+    DivImg.style.cursor="zoom-out";
+}
+function  openImg(e){
+    DivImg.style.display="flex";
+    imagenCompleta.src = e;
+}
+
+let audio = new Audio("sound.mp3");
+let btn = document.getElementById("button");
+let pause = document.querySelector(".pause");
+
+btn.addEventListener("click",function(){
+    audio.play();
+    pause.innerHTML = `<h1>⏸</h1>`;
+});
+pause.addEventListener("click",function(){
+    audio.pause();
+})
+AOS.init();
+
+
+
+    
